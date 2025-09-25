ca632b484356ccdb0e61fbe8868a28ee49df4928
trant <tranthai462004@gmail.com>
2025-09-26 05:18:39 +0700

    GD_login

---
+++ app/src/main/java/com/example/baitaptmhud/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.baitaptmhud;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends MainActivity {
+    EditText etEmail, etPassword;
+    Button btnLogin;
+    TextView tvGoToRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmailLogin);
+        etPassword = findViewById(R.id.etPasswordLogin);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvGoToRegister = findViewById(R.id.tvGoToRegister);
+
+        btnLogin.setOnClickListener(v -> {
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if(email.equals("admin@gmail.com") && password.equals("123456")){
+                Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                // Ví dụ chuyển sang màn hình chính
+                startActivity(new Intent(this, MainActivity.class));
+            } else {
+                Toast.makeText(this, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
+
